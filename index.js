3b7b3c02220000fdfc98eb7f9911e4f202809ef3
Nazar Kruk <nazarkruk@icloud.com>
2022-05-08 23:35:53 -0400

    commit tree 38e5872d540fb91cea69e386458f4937799e0510

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solution here
+const findMatching = function(array, checkName){
+
+    let matchingDriver = array.filter(elem => elem.toLowerCase() === checkName.toLowerCase())
+    return matchingDriver
+}
+
+function fuzzyMatch(array, checkName){
+    let matchingDriver = array.filter(elem => elem[1] === checkName[1])
+    return matchingDriver
+}
+
+const matchName = function(array, checkName){
+    let matchingDriver = array.filter(elem => elem.name === checkName)
+    return matchingDriver
+}
